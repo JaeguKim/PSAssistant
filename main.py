bea0d2417fc548f4f9bd803f7ef13476ae92c6fb
김재구 <rlaworn1993@gmail.com>
2020-05-28 20:16:52 +0900

    write comments of files of current directories or sub-directories to file

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import os
 
-metaFiles = ['result.txt','main.py']
+metaFiles = ['result.txt','main.py','.git']
 seperator = ''.join(['-']*30)
 
 def write(f_name,f_write):
     f_read = open(f_name,"r")
+    print(f_name)
     lines = f_read.read().split('\n')
     f_write.write(seperator+'\n')
     f_write.write(f_name+'\n')
@@ -24,11 +25,33 @@ def getFileList():
                 res.append(entry)
     return res
 
+def getDirectoryList():
+    basePath = './'
+    res = []
+    for entry in os.listdir(basePath):
+        if os.path.isdir(os.path.join(basePath,entry)):
+            res.append(entry)
+    return res
+
+def writeFilesRecursively(basePath,f_write):
+    #basePath = './'
+    for entry in os.listdir(basePath):
+        if entry in metaFiles:
+            continue
+        path = os.path.join(basePath,entry)
+        if os.path.isdir(path):
+            writeFilesRecursively(path,f_write)
+        elif os.path.isfile(path):
+            write(path,f_write)
+            
 def main():
     f_write = open("result.txt","w")
-    f_list = getFileList()
-    for f_name in f_list:
-        write(f_name,f_write)
+    # f_list = getFileList()
+    # for f_name in f_list:
+    #     write(f_name,f_write)
+    basePath = './'
+    writeFilesRecursively(basePath,f_write)
     f_write.close()
 
 main()
+#print(getDirectoryList())
