37e0d1bdc57ef4bdb324dfa5bc7d2d4a6f5d4fb5
김재구 <rlaworn1993@gmail.com>
2020-05-28 23:26:46 +0900

    program skips files having no comments and writes modified date of files

--- main.py
+++ main.py
@@ -1,57 +1,46 @@
 import os
+import datetime
 
-metaFiles = ['result.txt','main.py','.git']
+metaFiles = ['main.py']
+fExtensions = ['.py','.cpp']
 seperator = ''.join(['-']*30)
 
 def write(f_name,f_write):
     f_read = open(f_name,"r")
-    print(f_name)
+    comments = []
     lines = f_read.read().split('\n')
-    f_write.write(seperator+'\n')
-    f_write.write(f_name+'\n')
+    modifiedTime = os.path.getmtime(f_name) 
+    
     for line in lines:
         if '#' in line:
-            res = line.replace('#','')+'\n'
-            f_write.write(res)
-    f_write.write(seperator)
-    f_read.close()
-
-def getFileList():
-    basePath = './'
-    res = []
-    for entry in os.listdir(basePath):
-        if entry not in metaFiles:
-            if os.path.isfile(os.path.join(basePath,entry)):
-                res.append(entry)
-    return res
-
-def getDirectoryList():
-    basePath = './'
-    res = []
-    for entry in os.listdir(basePath):
-        if os.path.isdir(os.path.join(basePath,entry)):
-            res.append(entry)
-    return res
+            comment = line.replace('#','')+'\n'
+            comments.append(comment)
+    
+    if len(comments) > 0:
+        f_write.write(seperator+'\n')
+        value = datetime.datetime.fromtimestamp(modifiedTime)
+        f_write.write(f"{value:%Y-%m-%d %a %H:%M:%S}"+'\n')
+        f_write.write(f_name+'\n')
+        for comment in comments:
+            f_write.write(comment)
+        f_write.write(seperator)
+        f_read.close()
 
 def writeFilesRecursively(basePath,f_write):
     #basePath = './'
     for entry in os.listdir(basePath):
-        if entry in metaFiles:
-            continue
         path = os.path.join(basePath,entry)
         if os.path.isdir(path):
             writeFilesRecursively(path,f_write)
         elif os.path.isfile(path):
+            if os.path.splitext(entry)[-1] not in fExtensions or entry in metaFiles:
+                continue
             write(path,f_write)
             
 def main():
     f_write = open("result.txt","w")
-    # f_list = getFileList()
-    # for f_name in f_list:
-    #     write(f_name,f_write)
     basePath = './'
     writeFilesRecursively(basePath,f_write)
     f_write.close()
 
 main()
-#print(getDirectoryList())
---
+++ test/Codeforces/Drinks.py
@@ -0,0 +1,12 @@
+#4min
+n = int(input())
+arr = input().split()
+sum = 0
+for elem in arr:
+    sum += int(elem)
+print(sum/n)
+
+
+
+
+
